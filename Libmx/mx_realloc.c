963c54cb695bddc61c9223d71f80f89eb8d39928
Oleg Berdyshev <oberdyshev@e2r9p8.unit.ua>
2019-10-31 18:00:11 +0200

    makefile

---
+++ Libmx/mx_realloc.c
@@ -0,0 +1,25 @@
+#include "libmx.h"
+
+static int my_min(int a, int b);
+
+void *mx_realloc(void *ptr, size_t size) {
+    if (ptr == NULL)
+        return malloc(size);
+    if (size == 0) {
+        free(ptr);
+        return malloc(0);
+    }
+    
+    int sz = my_min(size, malloc_size(ptr));
+    void *p = malloc(size);
+    if (p == NULL) return NULL;
+ 
+    mx_memcpy(p, ptr, sz);
+    free(ptr);
+    return p;
+}
+
+static int my_min(int a, int b) {
+    return a < b ? a : b;
+}
+

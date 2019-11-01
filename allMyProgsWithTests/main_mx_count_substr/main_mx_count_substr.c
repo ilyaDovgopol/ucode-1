65af5ca1b2b603c0168d4e0f7fc17ffb4c15ac75
berdyshevol <berdyshevo@gmail.com>
2019-11-01 13:41:07 +0200

    bug in mx_count_substr

--- allMyProgsWithTests/main_mx_count_substr/main_mx_count_substr.c
+++ allMyProgsWithTests/main_mx_count_substr/main_mx_count_substr.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int mx_count_substr(const char *str, const char *sub);
 
 int main(void) {
     char s[] = "yo yoyoyo yo Ne o";
+    char subs[] = "yo";
+    //char subs[] = " ";
+    printf("case 1) result should be 5: %d\n", mx_count_substr(s, subs));
+    
+    char s1[] = "";
     //char subs[] = "yo";
-    char subs[] = " ";
-    printf("%d", mx_count_substr(s, subs));
+    char subs1[] = " ";
+    printf("case 2) result should be 0: %d\n", mx_count_substr(s1, subs1));
+
+    char s2[] = "   yo yoyoyo yo Ne o";
+    char subs2[] = "";
+    //char subs2[] = "";
+    printf("case 3) result should be -1: %d\n", mx_count_substr(s2, subs2));
 
     return 0;
 }
